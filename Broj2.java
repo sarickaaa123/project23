97495f0cc5acd8ff1675c2f64f9ea89c211abc67
Sara Usevska <sarausevska42@yahoo.com>
2023-12-03 17:41:40 +0100

    first commit

---
+++ Broj2.java
@@ -0,0 +1,13 @@
+package project_26;
+
+public class Broj2{
+
+	public static void main(String[] args) {
+	     myFunc(10);
+		}
+		public static void myFunc(int x) {
+		int y=x*3;
+		System.out.println(y);
+		}
+
+	}
